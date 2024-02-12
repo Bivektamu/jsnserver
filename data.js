7d42f624ecc7b417b45b8094645de4561900ff50
Bivek Gurung <bivek.tamu@gmail.com>
2024-02-12 18:57:17 +1100

    Server updated to handle specific page route

---
+++ data.js
@@ -0,0 +1,449 @@
+module.exports = {
+  "home": {
+    hero: {
+      carousels: [
+        {
+          imgSrc: "",
+          caption: "We create liveable spaces",
+        },
+        {
+          imgSrc: "",
+          caption:
+            "We specialise in residential, commercial and mixed-use property development",
+        },
+        {
+          imgSrc: "",
+          caption: "We maximise value for property owners and our investors",
+        },
+      ],
+
+      // carousels: [],
+      videoId: "901758877",
+      title: "",
+      caption: "We maximise value for property owners and our investors",
+      imgSrc: "/images/home/home_video01.jpg",
+    },
+    featuredLists: [
+      {
+        id: "1",
+        imgSrc: "/images/home/project_thumb01.jpg",
+        projectName: "PROJECT NAME",
+        location: "St Leonards",
+        alias: "details-1",
+      },
+      {
+        id: "2",
+        imgSrc: "/images/home/project_thumb02.jpg",
+        projectName: "PROJECT NAME",
+        location: "Gladesville",
+        alias: "details-2",
+      },
+      {
+        id: "3",
+        imgSrc: "/images/home/project_thumb03.jpg",
+        projectName: "AMAROO RIDGE ESTATE",
+        location: "Castle Cove",
+        alias: "details-3",
+      },
+    ],
+
+    exploreLists: [
+      {
+        imgSrc: "/images/home/e1.jpg",
+        caption: "Our Projects",
+        copy: "Our years of experience in development aligned with our in-house capability & resources enables us to deliver projects with maximum returns.",
+      },
+      {
+        imgSrc: "/images/home/e2.jpg",
+        caption: "Our Story",
+        copy: "Our ability to identify emerging trends and opportunities, is one of our key strengths and ensures the potential for growth with our investment partners",
+      },
+    ],
+
+    parallax: {
+      imgSrc: "/images/home/para.jpg",
+      caption:
+        "LApropertyCo. is a visionary property group driven to be market leaders, built on generations of experience.",
+    },
+    instaImages: [
+      { imgSrc: "/images/home/insta01.jpg", link: "" },
+      { imgSrc: "/images/home/insta02.jpg", link: "" },
+      { imgSrc: "/images/home/insta03.jpg", link: "" },
+      { imgSrc: "/images/home/insta04.jpg", link: "" },
+    ],
+  },
+
+  ////////////////////////////////////////////////////////////////////////////////////////
+  "project": {
+    hero: {
+      imgSrc: "/images/projects/hero.jpg",
+      title: "COLLECTION",
+      caption:
+        "LAProperty creates spaces of enduring quality, and delivers projects that are placemaking.",
+    },
+    projects: [
+      {
+        id: 1,
+        imgSrc: "/images/projects/project_thumb01.jpg",
+        videoId: "901758877",
+        alias: "details-1",
+        projectName: "AMAROO RIDGE ESTATE",
+        location: "St Leonards",
+        stage: "under construction",
+        status: "current",
+        value: "20m",
+        size: "11",
+        completionDate: "2024-06-30",
+        architect: "PBD Architects",
+        websiteURL: "http://someurl.com.au",
+        type: "houses",
+        contact: "1800221366",
+        heading: "Welcome to Amaroo Ridge Estate",
+        title: "Panoramic Living",
+        richText: `<p> Amaroo Ridge Estate rises atop Castle Cove, offering unparalleled vistas of the scenic
+              landscape and beyond. Castle Cove’s unique location allows connection with natural
+              surrounds yet residents are minutes from Sydney’s major hubs.</p>
+            <br />
+            <p>This boutique has only eleven residences - 2 & 3 bedroom units plus grand houses.</p>
+            <br />
+            <p>Amaroo Ridge Estate is peacefully still, giving a sense of calm to residents, whilst Castle Cove
+              Golf Club and all its amenities are on your doorstep.</p>
+            <br />
+            <p>Moments further to local stores and surrounding shopping centres, Amaroo Ridge Estates
+              location, nestled above Castle Cove, remains connected to the beating heart of the City.</p>`,
+        gallery: {
+          title: " PROJECT GALLERY",
+          caption: "",
+          images: [
+            {
+              imgSrc: "/images/project-details/amaroo_thumb01.jpg",
+            },
+            {
+              imgSrc: "/images/project-details/amaroo_thumb02.jpg",
+            },
+            {
+              imgSrc: "/images/project-details/amaroo_thumb03.jpg",
+            },
+            {
+              imgSrc: "/images/project-details/amaroo_thumb04.jpg",
+            },
+            {
+              imgSrc: "/images/project-details/amaroo_thumb05.jpg",
+            },
+            {
+              imgSrc: "/images/project-details/amaroo_thumb06.jpg",
+            },
+          ],
+        },
+        lifestyle: {
+          title: "LIFESTYLE",
+          caption:
+            "The peninsula suburb of Castle Cove is uniquely positioned amongst pristine bushland and the stunning waterways of Middle Harbour. Local activities of trail-walking, boating, golf, tennis, and cycling complement the suburbs vibrant yet intimate local community.",
+          images: [
+            {
+              imgSrc: "/images/project-details/lifestyle01.jpg",
+            },
+            {
+              imgSrc: "/images/project-details/lifestyle02.jpg",
+            },
+            {
+              imgSrc: "/images/project-details/lifestyle03.jpg",
+            },
+            {
+              imgSrc: "/images/project-details/lifestyle04.jpg",
+            },
+            {
+              imgSrc: "/images/project-details/lifestyle05.jpg",
+            },
+            {
+              imgSrc: "/images/project-details/lifestyle06.jpg",
+            },
+          ],
+        },
+        map: {
+          lat: "-33.86770092532593",
+          lng: "151.2061690839193",
+          marker: "",
+        },
+      },
+      {
+        id: 2,
+        imgSrc: "/images/projects/project_thumb02.jpg",
+        videoId: "901758877",
+        alias: "details-2",
+        projectName: "AMAROO RIDGE ESTATE",
+        location: "St Leonards",
+        stage: "under construction",
+        status: "current",
+        value: "20m",
+        size: "11",
+        completionDate: "2024-06-30",
+        architect: "PBD Architects",
+        websiteURL: "http://someurl.com.au",
+        type: "apartments",
+        contact: "1800221366",
+        heading: "Welcome to Amaroo Ridge Estate",
+        title: "Panoramic Living",
+        richText: `
+            <p> Amaroo Ridge Estate rises atop Castle Cove, offering unparalleled vistas of the scenic
+              landscape and beyond. Castle Cove’s unique location allows connection with natural
+              surrounds yet residents are minutes from Sydney’s major hubs.</p>
+            <br />
+            <p>This boutique has only eleven residences - 2 & 3 bedroom units plus grand houses.</p>
+            <br />
+            <p>Amaroo Ridge Estate is peacefully still, giving a sense of calm to residents, whilst Castle Cove
+              Golf Club and all its amenities are on your doorstep.</p>
+            <br />
+            <p>Moments further to local stores and surrounding shopping centres, Amaroo Ridge Estates
+              location, nestled above Castle Cove, remains connected to the beating heart of the City.</p>
+         `,
+        gallery: {
+          title: " PROJECT GALLERY",
+          caption: "",
+          images: [
+            {
+              imgSrc: "/images/project-details/amaroo_thumb01.jpg",
+            },
+            {
+              imgSrc: "/images/project-details/amaroo_thumb02.jpg",
+            },
+            {
+              imgSrc: "/images/project-details/amaroo_thumb03.jpg",
+            },
+            {
+              imgSrc: "/images/project-details/amaroo_thumb04.jpg",
+            },
+            {
+              imgSrc: "/images/project-details/amaroo_thumb05.jpg",
+            },
+            {
+              imgSrc: "/images/project-details/amaroo_thumb06.jpg",
+            },
+          ],
+        },
+        lifestyle: {
+          title: "LIFESTYLE",
+          caption:
+            "The peninsula suburb of Castle Cove is uniquely positioned amongst pristine bushland and the stunning waterways of Middle Harbour. Local activities of trail-walking, boating, golf, tennis, and cycling complement the suburbs vibrant yet intimate local community.",
+          images: [
+            {
+              imgSrc: "/images/project-details/lifestyle01.jpg",
+            },
+            {
+              imgSrc: "/images/project-details/lifestyle02.jpg",
+            },
+            {
+              imgSrc: "/images/project-details/lifestyle03.jpg",
+            },
+            {
+              imgSrc: "/images/project-details/lifestyle04.jpg",
+            },
+            {
+              imgSrc: "/images/project-details/lifestyle05.jpg",
+            },
+            {
+              imgSrc: "/images/project-details/lifestyle06.jpg",
+            },
+          ],
+        },
+        map: {
+          lat: "-33.86770092532593",
+          lng: "151.2061690839193",
+          marker: "/images/marker.svg",
+        },
+      },
+      {
+        id: 3,
+        imgSrc: "/images/projects/project_thumb03.jpg",
+        videoId: "901758877",
+        alias: "details-3",
+        projectName: "AMAROO RIDGE ESTATE",
+        location: "St Leonards",
+        stage: "under construction",
+        status: "completed",
+        value: "20m",
+        size: "11",
+        completionDate: "2024-06-30",
+        architect: "PBD Architects",
+        websiteURL: "http://someurl.com.au",
+        type: "commercial",
+        contact: "1800221366",
+        heading: "Welcome to Amaroo Ridge Estate",
+        title: "Panoramic Living",
+        richText: `
+            <p> Amaroo Ridge Estate rises atop Castle Cove, offering unparalleled vistas of the scenic
+              landscape and beyond. Castle Cove’s unique location allows connection with natural
+              surrounds yet residents are minutes from Sydney’s major hubs.</p>
+            <br />
+            <p>This boutique has only eleven residences - 2 & 3 bedroom units plus grand houses.</p>
+            <br />
+            <p>Amaroo Ridge Estate is peacefully still, giving a sense of calm to residents, whilst Castle Cove
+              Golf Club and all its amenities are on your doorstep.</p>
+            <br />
+            <p>Moments further to local stores and surrounding shopping centres, Amaroo Ridge Estates
+              location, nestled above Castle Cove, remains connected to the beating heart of the City.</p>
+         `,
+        gallery: {
+          title: " PROJECT GALLERY",
+          caption: "",
+          images: [
+            {
+              imgSrc: "/images/project-details/amaroo_thumb01.jpg",
+            },
+            {
+              imgSrc: "/images/project-details/amaroo_thumb02.jpg",
+            },
+            {
+              imgSrc: "/images/project-details/amaroo_thumb03.jpg",
+            },
+            {
+              imgSrc: "/images/project-details/amaroo_thumb04.jpg",
+            },
+            {
+              imgSrc: "/images/project-details/amaroo_thumb05.jpg",
+            },
+            {
+              imgSrc: "/images/project-details/amaroo_thumb06.jpg",
+            },
+          ],
+        },
+        lifestyle: {
+          title: "LIFESTYLE",
+          caption:
+            "The peninsula suburb of Castle Cove is uniquely positioned amongst pristine bushland and the stunning waterways of Middle Harbour. Local activities of trail-walking, boating, golf, tennis, and cycling complement the suburbs vibrant yet intimate local community.",
+          images: [
+            {
+              imgSrc: "/images/project-details/lifestyle01.jpg",
+            },
+            {
+              imgSrc: "/images/project-details/lifestyle02.jpg",
+            },
+            {
+              imgSrc: "/images/project-details/lifestyle03.jpg",
+            },
+            {
+              imgSrc: "/images/project-details/lifestyle04.jpg",
+            },
+            {
+              imgSrc: "/images/project-details/lifestyle05.jpg",
+            },
+            {
+              imgSrc: "/images/project-details/lifestyle06.jpg",
+            },
+          ],
+        },
+        map: {
+          lat: "-33.86770092532593",
+          lng: "151.2061690839193",
+          marker: "/images/marker.svg",
+        },
+      },
+      {
+        id: 4,
+        imgSrc: "/images/projects/project_thumb04.jpg",
+        videoId: "901758877",
+        alias: "details-4",
+        projectName: "AMAROO RIDGE ESTATE",
+        location: "St Leonards",
+        stage: "under construction",
+        status: "current",
+        value: "20m",
+        size: "11",
+        completionDate: "2024-06-30",
+        architect: "PBD Architects",
+        websiteURL: "http://someurl.com.au",
+        type: "commercial",
+        contact: "1800221366",
+        heading: "Welcome to Amaroo Ridge Estate",
+        title: "Panoramic Living",
+        richText: `
+            <p> Amaroo Ridge Estate rises atop Castle Cove, offering unparalleled vistas of the scenic
+              landscape and beyond. Castle Cove’s unique location allows connection with natural
+              surrounds yet residents are minutes from Sydney’s major hubs.</p>
+            <br />
+            <p>This boutique has only eleven residences - 2 & 3 bedroom units plus grand houses.</p>
+            <br />
+            <p>Amaroo Ridge Estate is peacefully still, giving a sense of calm to residents, whilst Castle Cove
+              Golf Club and all its amenities are on your doorstep.</p>
+            <br />
+            <p>Moments further to local stores and surrounding shopping centres, Amaroo Ridge Estates
+              location, nestled above Castle Cove, remains connected to the beating heart of the City.</p>
+         `,
+        gallery: {
+          title: " PROJECT GALLERY",
+          caption: "",
+          images: [
+            {
+              imgSrc: "/images/project-details/amaroo_thumb01.jpg",
+            },
+            {
+              imgSrc: "/images/project-details/amaroo_thumb02.jpg",
+            },
+            {
+              imgSrc: "/images/project-details/amaroo_thumb03.jpg",
+            },
+            {
+              imgSrc: "/images/project-details/amaroo_thumb04.jpg",
+            },
+            {
+              imgSrc: "/images/project-details/amaroo_thumb05.jpg",
+            },
+            {
+              imgSrc: "/images/project-details/amaroo_thumb06.jpg",
+            },
+          ],
+        },
+        lifestyle: {
+          title: "LIFESTYLE",
+          caption:
+            "The peninsula suburb of Castle Cove is uniquely positioned amongst pristine bushland and the stunning waterways of Middle Harbour. Local activities of trail-walking, boating, golf, tennis, and cycling complement the suburbs vibrant yet intimate local community.",
+          images: [
+            {
+              imgSrc: "/images/project-details/lifestyle01.jpg",
+            },
+            {
+              imgSrc: "/images/project-details/lifestyle02.jpg",
+            },
+            {
+              imgSrc: "/images/project-details/lifestyle03.jpg",
+            },
+            {
+              imgSrc: "/images/project-details/lifestyle04.jpg",
+            },
+            {
+              imgSrc: "/images/project-details/lifestyle05.jpg",
+            },
+            {
+              imgSrc: "/images/project-details/lifestyle06.jpg",
+            },
+          ],
+        },
+        map: {
+          lat: "-33.86770092532593",
+          lng: "151.2061690839193",
+          marker: "/images/marker.svg",
+        },
+      },
+    ],
+  },
+
+  "story": {
+    hero: {
+      imgSrc: "/images/projects/hero.jpg",
+      title: "Our Story",
+      caption:
+        "LAProperty creates spaces of enduring quality, and delivers projects that are placemaking.",
+    },
+
+    instaImages: [
+      { imgSrc: "/images/home/insta01.jpg", link: "" },
+      { imgSrc: "/images/home/insta02.jpg", link: "" },
+      { imgSrc: "/images/home/insta03.jpg", link: "" },
+      { imgSrc: "/images/home/insta04.jpg", link: "" },
+      { imgSrc: "/images/home/insta02.jpg", link: "" },
+      { imgSrc: "/images/home/insta01.jpg", link: "" },
+      { imgSrc: "/images/home/insta04.jpg", link: "" },
+      { imgSrc: "/images/home/insta03.jpg", link: "" },
+    ],
+  },
+};
--- functions/api.js
+++ functions/api.js
@@ -1,14 +1,20 @@
 const express =  require('express')
 const serverless =  require('serverless-http')
-
+const content = require('../data')
 const app = express()
 const router = express.Router()
-router.get('/', (req, res) => {
-    res.json({message: 'Hello World!'})
-})
-// app.get('/home', (req, res) => {
-//     res.json({message: 'This is homepage'})
-// })
 
+router.get(`/:page`, (req, res)=> {
+    try {
+        if(content && content[req.params.page]) {
+            res.status(200).json(content[req.params.page])
+        }
+        else {
+            throw new Error('Content not found')
+        }
+    } catch (error) {
+        res.status(500).json({"error": error.message})
+    }
+})
 app.use('/', router)
 module.exports.handler = serverless(app)
