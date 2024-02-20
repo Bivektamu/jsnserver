522fbc1f908970c75791cf8f891c0986c44209ea
Bivek Gurung <bivek.tamu@gmail.com>
2024-02-20 11:07:06 +1100

    bug fixed

--- functions/api.js
+++ functions/api.js
@@ -9,36 +9,30 @@ const router = express.Router();
 app.use(cors());
 app.use(bodyParser.json());
 // 
-// router.get(`/:page`, (req, res) => {
-//   try {
-//     if (content && content[req.params.page]) {
-//         console.log(content[req.params.page])
-//       res.status(200).json(content[req.params.page]);
-//     } else {
-//       throw new Error("Content not found");
-//     }
-//   } catch (error) {
-//     res.status(500).json({ error: error.message });
-//   }
-// });
-
-router.post(`/contact`, (req, res) => {
-    try {
-      if(req.body) {
-        console.log(req.body)
-          res.sendStatus(200)
-      }
-  
-      // if(content && content[req.params.page]) {
-      //     res.status(200).json(content[req.params.page])
-      // }
-      // else {
-      //     throw new Error('Content not found')
-      // }
-    } catch (error) {
-      res.status(500).json({ error: error.message });
+router.get(`/:page`, (req, res) => {
+  try {
+    if (content && content[req.params.page]) {
+        console.log(content[req.params.page])
+      res.status(200).json(content[req.params.page]);
+    } else {
+      throw new Error("Content not found");
     }
-  });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// router.post(`/contact`, (req, res) => {
+//     try {
+//       if(req.body) {
+//         console.log(req.body)
+//           res.sendStatus(200)
+//       }
+  
+//     } catch (error) {
+//       res.status(500).json({ error: error.message });
+//     }
+//   });
 
 app.use("/", router);
 module.exports.handler = serverless(app);
